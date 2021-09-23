d5f0f6edbb9b3682ac2b8046fba0668d94ef4b38
Aetf <aetf@unlimited-code.works>
2021-09-23 17:32:32 -0400

    Update authelia to 4.31.0

--- src/serving/authelia.ts
+++ src/serving/authelia.ts
@@ -142,7 +142,7 @@ export class Authelia extends pulumi.ComponentResource<AutheliaArgs> {
             enableServiceLinks: false,
             containers: [{
                 name: "authelia",
-                image: "ghcr.io/authelia/authelia:4.30.4",
+                image: "ghcr.io/authelia/authelia:4.31.0",
                 command: ["authelia"],
                 args: [
                     `--config=${configPath}/authelia.yaml`
