078d70befb21fb00fd8e0fb8de0f03d6bbfe723f
Aetf <aetf@unlimited-code.works>
2023-09-25 21:34:31 -0700

    intel-gpu: enable sharing

--- src/base-cluster/intel-gpu.ts
+++ src/base-cluster/intel-gpu.ts
@@ -56,6 +56,9 @@ export class IntelDevicePlugins extends pulumi.ComponentResource<IntelDevicePlug
             fetchOpts: {
                 repo: "https://intel.github.io/helm-charts",
             },
+            values: {
+                sharedDevNum: 5,
+            },
         }, { parent: this });
     }
 }
