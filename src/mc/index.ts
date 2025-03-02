cd173e68a15d0ca907915b654bb6b90c618ed850
Aetf <aetf@unlimited-code.works>
2025-03-01 16:28:31 -0800

    minecraft: use rclone and google object storage directly for backup

--- src/mc/index.ts
+++ src/mc/index.ts
@@ -23,6 +23,13 @@ export class Mc extends pulumi.ComponentResource<McArgs> {
                 }
             }
         }, { parent: this });
+        const rcloneSecret = new SealedSecret(`${name}-rclone`, {
+            spec: {
+                encryptedData: {
+                    "rclone.conf": "AgCFc+18AaMXgJF0ghbqKO9nR0JOknFdyZz23lzMYdb/TOoPJrfq4FJgkoMah9+KmtjeeJraQ5jRJv5f75vIw+lPMqEbCgUSE8jadxE5TsjxSV6rFdIYD+8/zHeAfn7nGMtuyeFJizglJmVT8DJq+XcP9N8avN1o+y6T4OFBRAQixsbyGxBlMhGDmKgmQcjT87aMQJayAaboy1JbUzDybPtjan2l04kyUztp2v6wdqi3qQkcETNq04R591epzM6RXzhL2Nw/Zj2jrOJJp9zTbCh5kUxEQT5afG747yA9pAZ2QSQjwYCuTrNCUtxBohfbm/sKMVgE+mcEu+dEqCTWdXtZjr5sLcuG6RketepFMz9o6rYt5imUBYRjZHpPpumDvUjgpm+sqD8z0eZ7ppL3tCP/zOZnr1WDDC/ZySC3fuvJiu3ziQcsgqJUF7l7t5ZPNZWDhnqlOKM7FlScB0k8Y2M/Icn0hr6OMQQxzskiNNI/pTc41uLOmGJ1dbdpclBhFAQyy6bQC1WyKNvHlMSntv9iD9JP/qpWxvJTvkl9E7jFlnh04ZDl7NCixe26r0d6DKrZaDWnYiNXm7w2cx8mguIyL0js4CX26qo2NakeKyk411BPKblkgpQJCs0ZIRQbfgmucfjUP/GxJgkWKmWqk3u0iwK9oPDPb+aC9r+tXix3hNlR9L8sFT8OoOSn0ZkuYTZMmtY1oVHWh3uBim3JQ8g3taqfejejQlAO7Bq5ehKr/5nPzfchJ+CnVdcKc9Z9RCg/0P540T8s01zoCd3gzFZIFLpxezODPXQwRib+8/YjeYhuMLbGeupGW+n40j8bv6cVRByA5S8/9AFcQYvnjPoYAwKZgVjkUkM/nJbZ/5XFEo599uGztUTtupm8IHVjb5B57QjA7y3ZZlX2lpM/mVIEYjbOjQN/9MZf8Zm42bdbzrvKx4Ax+DBsmdzhjggsRE9yWV5LoGZG6MdyEBdK9TJV2CM0wFzIW/G9xwbX4M6rBsq7jkpiZVhMaGqS4VDcUfdrEsAnu7moBA==",
+                }
+            }
+        }, { parent: this });
 
         const configCm = new ConfigMap(name, {
             ref_file: __filename,
@@ -37,7 +44,7 @@ export class Mc extends pulumi.ComponentResource<McArgs> {
                     storage: "12Gi"
                 }
             }
-        }, { parent: this, retainOnDelete: true, protect: false });
+        }, { parent: this, retainOnDelete: true, protect: true });
 
         const backupPvc = args.base.createLocalStoragePVC(`${name}-backup`, {
             storageClassName: args.base.jfsStorageClass.metadata.name,
@@ -46,7 +53,7 @@ export class Mc extends pulumi.ComponentResource<McArgs> {
                     storage: "50Gi"
                 }
             }
-        }, { parent: this, retainOnDelete: true, protect: false });
+        }, { parent: this, retainOnDelete: false, protect: false });
 
         this.namespace = pvc.metadata.namespace;
         this.chart = new HelmChart(name, {
@@ -176,12 +183,10 @@ export class Mc extends pulumi.ComponentResource<McArgs> {
                     enabled: true,
                     pauseIfNoPlayers: "true",
                     compressMethod: "zstd",
-                    persistence: {
-                        backupDir: {
-                            enabled: true,
-                            "existingClaim": backupPvc.metadata.name,
-                        }
-                    }
+                    rcloneRemote: "fabric-science-mc-backup:rclone-fabric-science-mc",
+                    rcloneDestDir: "/",
+                    rcloneCompressMethod: "zstd",
+                    rcloneConfigExistingSecret: rcloneSecret.metadata.name,
                 }
             }
         }, { parent: this });
