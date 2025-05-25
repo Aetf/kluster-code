543fafb4613bd00c5622b095df381da9ef83d66a
Aetf <aetf@unlimited-code.works>
2025-05-24 18:45:09 -0700

    monitoring: add full prometheus/grafana stack

--- src/mon/index.ts
+++ src/mon/index.ts
@@ -23,101 +23,87 @@ export class Prometheus extends pulumi.ComponentResource<PrometheusArgs> {
             namespace,
         }, { parent: this });
 
-        const alertpvc = args.serving.base.createLocalStoragePVC(`${name}-alertmanager`, {
-            storageClassName: args.serving.base.localStorageClass.metadata.name,
-            resources: {
-                requests: {
-                    storage: "2Gi"
-                }
-            }
-        }, { parent: this, });
-        const pvc = args.serving.base.createLocalStoragePVC(name, {
-            storageClassName: args.serving.base.localStorageClass.metadata.name,
-            resources: {
-                requests: {
-                    storage: "4Gi"
-                }
-            }
-        }, { parent: this, });
-
         this.chart = new HelmChart(name, {
             namespace,
-            chart: "prometheus",
-            version: "15.12.2",
+            chart: "kube-prometheus-stack",
             values: {
                 nameOverride: name,
+                grafana: {
+                    resources: {
+                        requests: { cpu: "500m", memory: "512Mi" },
+                        limits: { cpu: "500m", memory: "512Mi" },
+                    },
+                    testFramework: { enabled: false },
+                },
                 alertmanager: {
-                    persistentVolume: {
-                        existingClaim: alertpvc.metadata.name,
-                    }
-                },
-                server: {
-                    name: "server",
-                    // the schema prefix is necessary, otherwise prometheus
-                    // intepretates the setting as a url prefix, not hostname.
-                    baseURL: 'https://' + args.host,
-                    // enable TLS directly on Prometheus
-                    extraArgs: {
-                        'web.config.file': '/etc/config/web.config.yml',
-                    },
-                    // mount per pod tls cert
-                    extraSecretMounts: [
-                        {
-                            name: 'tls',
-                            mountPath: '/tls',
-                            secretName: this.certificate.secretName,
-                            readOnly: true
-                        },
-                    ],
-                    // enable restart on cert change
-                    deploymentAnnotations: {
-                        "reloader.stakater.com/search": "true"
-                    },
-                    // the probe endpoint also needs to be HTTPS now
-                    probeScheme: 'HTTPS',
-                    // make the service listen on 443 rather than 80
-                    service: {
-                        servicePort: 443,
-                    },
-                    // storage
-                    persistentVolume: {
-                        existingClaim: pvc.metadata.name,
-                    },
-                    // with rolling update, the old ReplicaSet is kept before
-                    // creating new one, causing lock issue on the storage
-                    strategy: {
-                        type: 'Recreate',
-                    },
-                    // enable restart on cert change
-                    podAnnotations: {
-                        "reloader.stakater.com/search": "true"
-                    },
-                },
-                serverFiles: {
-                    "web.config.yml": {
-                        "tls_server_config": {
-                            cert_file: "/tls/tls.crt",
-                            key_file: "/tls/tls.key",
+                    alertmanagerSpec: {
+                        storage: {
+                            volumeClaimTemplate: {
+                                spec: {
+                                    storageClassName: args.serving.base.localStorageClass.metadata.name,
+                                    accessModes: ["ReadWriteOnce"],
+                                    resources: {
+                                        requests: {
+                                            storage: "1Gi",
+                                        }
+                                    }
+                                }
+                            },
                         },
                     },
                 },
-                extraScrapeConfigs: pulumi.all([namespace]).apply(([ns]) => {
-                    return dedent`
-                    - job_name: prometheus-secure
-                      scheme: https
-                      tls_config:
-                        ca_file: /tls/ca.crt
-                      static_configs:
-                      - targets:
-                        - ${name}-server.${ns}
-                    `;
-                })
+                prometheus: {
+                    prometheusSpec: {
+                        storageSpec: {
+                            volumeClaimTemplate: {
+                                spec: {
+                                    storageClassName: args.serving.base.localStorageClass.metadata.name,
+                                    accessModes: ["ReadWriteOnce"],
+                                    resources: {
+                                        requests: {
+                                            storage: "4Gi",
+                                        }
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+                prometheusOperator: {
+                    resources: {
+                        requests: { cpu: "100m", memory: "128Mi" },
+                        limits: { cpu: "200m", memory: "256Mi" },
+                    },
+                    admissionWebhooks: {
+                        enabled: true,
+                        certManager: {
+                            enabled: true,
+                            issuerRef: {
+                                name: args.serving.base.rootIssuer.metadata.name,
+                                kind: "ClusterIssuer",
+                            }
+                        }
+                    },
+                },
+                "kube-state-metrics": {
+                    resources: {
+                        requests: { cpu: "10m", memory: "32Mi" },
+                        limits: { cpu: "100m", memory: "64Mi" },
+                    },
+                },
+                "prometheus-node-exporter": {
+                    resources: {
+                        requests: { cpu: "10m", memory: "32Mi" },
+                        limits: { cpu: "200m", memory: "64Mi" },
+                    },
+                },
             },
         }, { parent: this });
 
         args.serving.createFrontendService(name, {
             host: args.host,
-            targetService: this.chart.service(new RegExp(`${name}-server$`)),
+            targetService: this.chart.service(new RegExp(`grafana`)),
+            enableTls: false,
             enableAuth: true,
         });
     }
